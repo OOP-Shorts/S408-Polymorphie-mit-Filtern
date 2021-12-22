06bece6f797800f53ee5269f50637d996516c35b
Lukas Schauhuber <35468672+luggaesch@users.noreply.github.com>
2021-12-22 11:58:58 +0100

    startercode hinzugefügt

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    private static final Animal[] ANIMAL_LIST = {
+            new Dog("Rex", 5, "German Shepherd"),
+            new Cat("Fluffy", 2, true),
+            new Cat("Miezi", 13, false),
+            new Cat("Moritz", 4, false),
+            new Dog("Fiffy", 9, "Border Collie")
+    };
+
+    public static void main(String[] args) {
+    }
+
+    /*
+        Ergänze hier die neue Methode 'removeCatsFromList'.
+     */
+}
